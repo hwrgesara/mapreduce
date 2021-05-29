67d62658577a4f85c37c4695465f6b62613f8ebe
gesara12 <s_halili20@stud.hwr-berlin.de>
2021-05-29 14:31:10 +0200

    changes exercise 3

--- mapper.py
+++ mapper.py
@@ -33,7 +33,7 @@ for line in sys.stdin:
     # strip removes whitespaces and new lines at the beginning and end of the line
     # the result is a tuple with 6 elements
     data = line.strip().split("\t")
-    if len(tuple) < 6:
+    if len(data) < 6:
         sys.stdout.write("Error: There are less than 6 elements in tuple")
     else:
         # store the 6 elements of the tuple in seperate variables
