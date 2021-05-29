e278efd5b2ddb00fd718ca6e88cb76f3999b0a0b
gesara12 <s_halili20@stud.hwr-berlin.de>
2021-05-29 15:04:36 +0200

    part 4 changes         # Write the key-value combination to standard output (stdout)         # Key is the payment, value is the sales         # Key is the payment, value is the sales         # With a tab (\t) between key and value         # New line \n means new record         sys.stdout.write("{0}\t{1}\n".format(category, sales))

--- mapper.py
+++ mapper.py
@@ -36,14 +36,15 @@ for line in sys.stdin:
     if len(data) < 6:
         sys.stdout.write("Error: There are less than 6 elements in tuple")
     else:
-        # store the 6 elements of the tuple in seperate variables
         date, time, item, category, sales, payment = data
 
-    if (category == "Cameras"):
+    if (category in ("Computers", "Cameras", "Video Games")):
         # Write the key-value combination to standard output (stdout)
+        # Key is the payment, value is the sales     
         # Key is the payment, value is the sales
         # With a tab (\t) between key and value
         # New line \n means new record
         sys.stdout.write("{0}\t{1}\n".format(category, sales))
+
 
 
